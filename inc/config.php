29409f145621fa70f93e7527711285dea6affa11
Hollick <h0k4ch4n@gmail.com>
2017-04-28 19:07:17 +0200

    Update config.php
    Well I'm just putting this info out there for those that need tor protection. no reason to pull this if not stable.

--- inc/config.php
+++ inc/config.php
@@ -197,10 +197,10 @@
 	// Prevents most Tor exit nodes from making posts. Recommended, as a lot of abuse comes from Tor because
 	// of the strong anonymity associated with it.
 	// Example: $config['dnsbl'][] = 'another.blacklist.net'; // 
-	$config['dnsbl'][] = array('tor.dnsbl.sectoor.de', 1); //sectoor.de site is dead. the number stands for (an) ip adress(es) I guess. 
+	// $config['dnsbl'][] = array('tor.dnsbl.sectoor.de', 1); //sectoor.de site is dead. the number stands for (an) ip adress(es) I guess. 
 	
 	// Replacement for sectoor.de
-	// $config['dnsbl'][] = 'torexit.dan.me.uk';
+	$config['dnsbl'][] = 'torexit.dan.me.uk';
 
 	// http://www.sorbs.net/using.shtml
 	// $config['dnsbl'][] = array('dnsbl.sorbs.net', array(2, 3, 4, 5, 6, 7, 8, 9));
