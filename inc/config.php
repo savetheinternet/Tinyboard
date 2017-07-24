4025705eaca0d8025ae09ae93564ecb350b6a72b
czaks <marcin@6irc.net>
2017-07-24 15:04:01 -0400

    replace faulty dnsbl with efnetrbl

--- inc/config.php
+++ inc/config.php
@@ -200,7 +200,7 @@
 	// $config['dnsbl'][] = array('tor.dnsbl.sectoor.de', 1); //sectoor.de site is dead. the number stands for (an) ip adress(es) I guess. 
 	
 	// Replacement for sectoor.de
-	$config['dnsbl'][] = 'torexit.dan.me.uk';
+	$config['dnsbl'][] = array('rbl.efnet.org', 4);
 
 	// http://www.sorbs.net/using.shtml
 	// $config['dnsbl'][] = array('dnsbl.sorbs.net', array(2, 3, 4, 5, 6, 7, 8, 9));
