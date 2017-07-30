7aeac11460dc5aa8e8ad468671a7a29c95feac64
roIyat <30560801+roIyat@users.noreply.github.com>
2017-07-30 02:57:39 +0200

    Update thread-watcher.js
    A watch thread button is now placed before reply button so citation links don't go between them.

--- js/thread-watcher.js
+++ js/thread-watcher.js
@@ -150,8 +150,8 @@ $(document).ready(function(){
 
 	//Append the watchlist toggle button.
 	$('.boardlist').append('<span>[ <a class="watchlist-toggle" href="#">'+_('watchlist')+'</a> ]</span>');
-	//Append a watch thread button after every OP.
-	$('.op>.intro').append('<a class="watchThread" href="#">['+_('Watch Thread')+']</a>');
+    	//Append a watch thread button after every OP post number.
+    	$('.op>.intro>.post_no:odd').after('<a class="watchThread" href="#">['+_('Watch Thread')+']</a>');
 
 	//Draw the watchlist, hidden.
 	watchlist.render();
