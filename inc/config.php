af06cf37378806e7718a56a7a439b06bc4250514
Fredrick Brennan <copypaste@kittens.ph>
2020-01-15 11:36:51 +0800

    Disable `check_updates` by default
    It no longer works and never will again, so...

--- inc/config.php
+++ inc/config.php
@@ -37,7 +37,7 @@
 	$config['blotter'] = &$config['global_message'];
 
 	// Automatically check if a newer version of Tinyboard is available when an administrator logs in.
-	$config['check_updates'] = true;
+	$config['check_updates'] = false;
 	// How often to check for updates
 	$config['check_updates_time'] = 43200; // 12 hours
 
