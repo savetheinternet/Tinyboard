f62acd5c1a6a9d1eff8a0ab1d6cc7551a9ca1c0e
runit <runithawkins@gmail.com>
2022-09-27 09:01:54 -0400

    banned.php now shows a pretty page instead of an ugly one. credits to JamesJDillon @ Lainchan

--- banned.php
+++ banned.php
@@ -1,7 +1,16 @@
 <?php
- require_once 'inc/bootstrap.php';
- checkBan();
- print "<!doctype html><html><head><meta charset='utf-8'><title>"._("Banned?")."</title></head><body>";
- print "<h1>"._("You are not banned.")."</h1>";
- print "</body></html>";
+  require_once 'inc/functions.php';
+  require_once 'inc/bans.php';
+  require_once 'inc/bootstrap.php';
+  checkBan();
+
+  //If the user is not banned, show the "not banned" page.
+  die(
+    Element('page.html', array(
+      'title' => _('Not banned!'),
+      'config' => $config,
+      'nojavascript' => true,
+      'body' => Element('notbanned.html', array()
+    ))
+  )); 
 ?>
