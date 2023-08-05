ea08c250fa568a09994cf85468184ecffc9610f5
RealAngeleno <angeleno@screamer.wiki>
2023-08-05 15:32:46 -0700

    Add mod.php logic

--- js/catalog-link.js
+++ js/catalog-link.js
@@ -13,46 +13,51 @@
  */
 
 function catalog() {
-var board = $("input[name='board']");
+    var board = $("input[name='board']");
+    var boardValue = board.first().val();
 
-var catalog_url = configRoot + board.first().val() + "/catalog.html";
+    var catalog_url = '';
+    if (window.location.href.includes('mod.php?/')) {
+        catalog_url = configRoot + 'mod.php?/' + boardValue + '/catalog.html';
+    } else {
+        catalog_url = configRoot + boardValue + '/catalog.html';
+    }
 
-var pages = document.getElementsByClassName('pages')[0];
-var bottom = document.getElementsByClassName('boardlist bottom')[0]
-var subtitle = document.getElementsByClassName('subtitle')[0];
+    var pages = document.getElementsByClassName('pages')[0];
+    var bottom = document.getElementsByClassName('boardlist bottom')[0];
+    var subtitle = document.getElementsByClassName('subtitle')[0];
 
-var link = document.createElement('a');
-link.href = catalog_url;
+    var link = document.createElement('a');
+    link.href = catalog_url;
 
-if (pages) {
-	link.textContent = _('Catalog');
-	link.style.color = '#F10000';
-	link.style.padding = '4px';
-	link.style.paddingLeft = '9px';
-	link.style.borderLeft = '1px solid'
-	link.style.borderLeftColor = '#A8A8A8';
-	link.style.textDecoration = "underline";
+    if (pages) {
+        link.textContent = _('Catalog');
+        link.style.color = '#F10000';
+        link.style.padding = '4px';
+        link.style.paddingLeft = '9px';
+        link.style.borderLeft = '1px solid';
+        link.style.borderLeftColor = '#A8A8A8';
+        link.style.textDecoration = "underline";
 
-	pages.appendChild(link)
-}
-else {
-	link.textContent = '['+_('Catalog')+']';
-	link.style.paddingLeft = '10px';
-	link.style.textDecoration = "underline";
-	document.body.insertBefore(link, bottom);
-}
+        pages.appendChild(link);
+    } else {
+        link.textContent = '['+_('Catalog')+']';
+        link.style.paddingLeft = '10px';
+        link.style.textDecoration = "underline";
+        document.body.insertBefore(link, bottom);
+    }
 
-if (subtitle) { 
-	var link2 = document.createElement('a');
-	link2.textContent = _('Catalog');
-	link2.href = catalog_url;
+    if (subtitle) { 
+        var link2 = document.createElement('a');
+        link2.textContent = _('Catalog');
+        link2.href = catalog_url;
 
-	var br = document.createElement('br');
-	subtitle.appendChild(br);
-	subtitle.appendChild(link2);	
-}
+        var br = document.createElement('br');
+        subtitle.appendChild(br);
+        subtitle.appendChild(link2);    
+    }
 }
 
 if (active_page == 'thread' || active_page == 'index') {
-	$(document).ready(catalog);
+    $(document).ready(catalog);
 }
